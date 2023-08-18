cb20c078cc1d2ffe8a34f6e62ccc9dc731cdaa0c
Raj Shekhar <channiecx954@gmail.com>
2023-08-18 23:53:02 +0530

    Fix width for smaller screens

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,9 +5,9 @@ function Footer({ todos }) {
     <div className='fixed h-20 bottom-0 w-full bg-emerald-400 flex justify-center items-center italic font-bold text-amber-900 text-center'>
       You have {todos.length} items on your list
       {todos.length > 0
-        ? ` and you have already packed ${complete} (${
-            Math.floor(complete / todos.length) * 100
-          }%)`
+        ? ` and you have already packed ${complete} (${Math.floor(
+            (complete / todos.length) * 100
+          )}%)`
         : ""}
     </div>
   );
